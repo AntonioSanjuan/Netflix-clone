7f9ee6c04ff39ce0adb97a82432de679fa5e5a48
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2020-12-01 22:12:09 +0000

    fix: added animations and material style

--- TestApp/WebApplication2/ClientApp/src/app/app.module.ts
+++ TestApp/WebApplication2/ClientApp/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { FlexLayoutModule } from '@angular/flex-layout/typings/module';
 
 @NgModule({
   declarations: [
@@ -18,6 +22,14 @@ import { LoginComponent } from './pages/login/login.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    // FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
