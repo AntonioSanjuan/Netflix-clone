7cf89d460bb6ee716aac33d35335b04fc27fe647
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2020-12-03 13:46:47 +0000

    feat: added flexLayout

--- TestApp/WebApplication2/ClientApp/src/app/app.module.ts
+++ TestApp/WebApplication2/ClientApp/src/app/app.module.ts
@@ -1,35 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { LoginComponent } from './pages/login/login.component';
-import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSliderModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { PAGES } from './pages';
 import { COMPONENTS } from './components';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const staticImports: any[] = [
-  //FlexLayoutModule,
+  FlexLayoutModule,
   MaterialModule
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,
     ...PAGES,
-    ...COMPONENTS
+    ...COMPONENTS,
+    AppComponent
   ],
   imports: [
     staticImports,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     BrowserAnimationsModule,
-    
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
     ])
