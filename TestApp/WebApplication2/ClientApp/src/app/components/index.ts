192606e4e99036f80079de8713e171b82b5b7fda
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2020-12-05 21:47:09 +0000

    feat: added css to several components and lot of designs

--- TestApp/WebApplication2/ClientApp/src/app/components/index.ts
+++ TestApp/WebApplication2/ClientApp/src/app/components/index.ts
@@ -1,5 +1,7 @@
+import { NavMenuComponent } from "./nav-bar/components/nav-menu/nav-menu.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 export const COMPONENTS: any[] = [
-    NavBarComponent
+    NavBarComponent,
+    NavMenuComponent
 ];
