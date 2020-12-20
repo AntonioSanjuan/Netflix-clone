477ed952787a0614cbc29df070eb5e54759c96bd
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2020-12-20 12:13:26 +0000

    navBar component

--- TestApp/WebApplication2/ClientApp/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ TestApp/WebApplication2/ClientApp/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,9 +1,45 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+import { ILoginResponse } from 'src/app/models/user-models/Login/LoginResponse.model';
+import { UtilService } from 'src/app/services/util/utils.service';
+import { NavBarComponent } from './nav-bar.component';
+
 describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture;
+
+  let authServiceStub;
+  const loginMock = {} as ILoginResponse;
+  let isAuthenticatedValueMock = false;
+
   beforeEach(() => {
+    isAuthenticatedValueMock = false;
+
+    authServiceStub = {
+        login : jest.fn(() => new Promise((resolve, reject) => resolve({})).then(() => loginMock) ),
+        getIsAuthenticated : jest.fn(() => isAuthenticatedValueMock )
+    };
+
+    TestBed.configureTestingModule({
+      declarations: [NavBarComponent],
+      imports: [],
+      providers: [
+        {provide: UtilService, useValue: authServiceStub},
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
   });
 
+  fixture = TestBed.createComponent(NavBarComponent);
+  component = fixture.componentInstance;
 
-  it('should create', () => {
-    expect(true).toBeTruthy();
+  fixture.detectChanges();
+  });
+
+  it('initially AuthService.getIsAuthenticated() should be FALSE', () => {
+    // spy
+    const actual = component.isAuthenticated;
+
+    expect(actual).toBeFalse();
   });
 });
--- TestApp/WebApplication2/ClientApp/src/app/components/nav-bar/nav-bar.component.ts
+++ TestApp/WebApplication2/ClientApp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/services/user/Auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
+  isAuthenticated: boolean;
+  isAuthenticatedSubsciption: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+    ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.getIsAuthenticated();
+    this.isAuthenticatedSubsciption = this.authService.getIsAuthenticated$().subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll();
+  }
+
+  private unsubscribeAll(): void {
+    this.isAuthenticatedSubsciption.unsubscribe();
   }
 
 }
