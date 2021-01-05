0a062bc3f5f1b232cff90d448c3d3c84dc92367b
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2021-01-05 19:35:15 +0000

    feat: added test proyect

---
+++ TestApp/MovieApiTest/Controllers/MovieControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace MovieApiTest.Controllers
+{
+    public class MovieControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TopTatedMovies()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestApp/MovieApiTest/Controllers/UserCotrollerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using Moq;
+using MovieApi.Controllers;
+using MovieApi.Models.AppSettings;
+using MovieApi.Models.User.Login.Request;
+using MovieApi.Models.User.Login.Response;
+using MovieApi.services.interfaces;
+using NUnit.Framework;
+namespace MovieApiTest.Controllers
+{
+    public class UserCotrollerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //dependencies (mock)
+            var mockUserService = new Mock<IUserService>();
+            var mockMoviedbSettings = new Mock<IOptions<TheMoviedbSettingsModel>>();
+
+            LoginRequestModel loginRequest = null;
+            LoginResponseModel loginResponse = null;
+            mockUserService.Setup(repo => repo.Login(loginRequest))
+                .ReturnsAsync(loginResponse);
+
+            var controller = new UserController(mockMoviedbSettings.Object, mockUserService.Object);
+        }
+
+        [Test]
+        public void Login()
+        {
+        }
+    }
+}
+
