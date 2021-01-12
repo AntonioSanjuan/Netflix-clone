a53903722b973ce027ed091a38c3bc49fbf1ee0a
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2021-01-12 09:50:47 +0000

    test: empty service tests

---
+++ TestApp/MovieApiTest/Services/MovieServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MovieApiTest.Services
+{
+    public class MovieServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetTopRatedMovies()
+        {
+        }
+
+        [Test]
+        public void GetTopRatedMovieImages()
+        {
+        }
+    }
+}
---
+++ TestApp/MovieApiTest/Services/UserServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MovieApiTest.Services
+{
+    public class UserServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Login()
+        {
+        }
+
+        [Test]
+        public void CreateRequestToken()
+        {
+        }
+    }
+}
