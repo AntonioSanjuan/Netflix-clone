359ef1f70ff3a7202adf44d7cd5af3193bcec9bd
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2021-01-12 09:50:27 +0000

    test: controller tests

--- TestApp/MovieApiTest/Controllers/MovieControllerTest.cs
+++ TestApp/MovieApiTest/Controllers/MovieControllerTest.cs
@@ -2,30 +2,38 @@
 using MovieApi.Controllers;
 using MovieApi.Models.Movie.GetTopTatedMovies.Request;
 using MovieApi.Models.Movie.GetTopTatedMovies.Response;
+using MovieApi.Models.User.Login.Request;
 using MovieApi.services.interfaces;
 using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace MovieApiTest.Controllers
 {
     public class MovieControllerTest
     {
+        Mock<IMovieService> _mockMovieService;
+        MovieController _controller;
         [SetUp]
         public void Setup()
         {
-            var mockMovieService = new Mock<IMovieService>();
+            _mockMovieService = new Mock<IMovieService>();
 
             TopRatedMoviesRequestModel loginRequest = null;
             TopRatedMoviesResponseModel loginResponse = null;
-            mockMovieService.Setup(repo => repo.GetTopRatedMovies(loginRequest))
+            _mockMovieService.Setup(repo => repo.GetTopRatedMovies(loginRequest))
                 .ReturnsAsync(loginResponse);
 
-            var controller = new MovieController(mockMovieService.Object);
+            _controller = new MovieController(_mockMovieService.Object);
         }
 
         [Test]
-        public void TopTatedMovies()
+        public async Task TopTatedMovies()
         {
-            Assert.Pass();
+            TopRatedMoviesRequestModel getTopRatedMoviesRequest = new TopRatedMoviesRequestModel();
+            await _controller.GetTopTatedMovies(getTopRatedMoviesRequest);
+
+            //assert
+            _mockMovieService.Verify(spy => spy.GetTopRatedMovies(getTopRatedMoviesRequest), Times.Once());
         }
     }
 }
--- TestApp/MovieApiTest/Controllers/UserCotrollerTest.cs
+++ TestApp/MovieApiTest/Controllers/UserCotrollerTest.cs
@@ -2,31 +2,54 @@
 using Moq;
 using MovieApi.Controllers;
 using MovieApi.Models.AppSettings;
+using MovieApi.Models.TheMoviedb.Auth.CreateRequestToken.Response;
 using MovieApi.Models.User.Login.Request;
 using MovieApi.Models.User.Login.Response;
 using MovieApi.services.interfaces;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
 namespace MovieApiTest.Controllers
 {
     public class UserCotrollerTest
     {
+        Mock<IUserService> _mockUserService;
+        CreateRequestTokenResponseModel _createRequestTokenResponse = new CreateRequestTokenResponseModel();
+        LoginResponseModel _loginResponse = new LoginResponseModel();
+
+        UserController _controller;
+
         [SetUp]
         public void Setup()
         {
-            //dependencies (mock)
-            var mockUserService = new Mock<IUserService>();
+
+            //dependencies (mock) + spy
+            _mockUserService = new Mock<IUserService>();
 
             LoginRequestModel loginRequest = null;
-            LoginResponseModel loginResponse = null;
-            mockUserService.Setup(repo => repo.Login(loginRequest))
-                .ReturnsAsync(loginResponse);
 
-            var controller = new UserController(mockUserService.Object);
+
+            _mockUserService.Setup(repo => repo.Login(loginRequest))
+                .ReturnsAsync(_loginResponse);
+            _mockUserService.Setup(repo => repo.CreateRequestToken())
+                .ReturnsAsync(_createRequestTokenResponse);
+
+            _controller = new UserController(_mockUserService.Object);
         }
 
         [Test]
-        public void Login()
+        public async Task Login()
         {
+            LoginRequestModel loginRequest = new LoginRequestModel{
+                user = "",
+                pass = ""
+            };
+            
+            await _controller.Login(loginRequest);
+
+            //assert
+            _mockUserService.Verify(loginSpy => loginSpy.Login(loginRequest), Times.Once());
         }
     }
 }
