95f7fffcc4650c141be24acec0cd849a3012f326
Antonio Sanjuan Prieto <antonio.sanjuan@ultebra.eu>
2021-01-14 14:36:57 +0000

    fix: fixed injection of httpClient into services

--- TestApp/MovieApiTest/Services/UserServiceTest.cs
+++ TestApp/MovieApiTest/Services/UserServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Options;
 using Moq;
+using Moq.Protected;
 using MovieApi.adapters.interfaces;
 using MovieApi.Models.AppSettings;
 using MovieApi.Models.TheMoviedb.Auth.CreateRequestToken.Response;
@@ -7,16 +8,21 @@ using MovieApi.Models.User.Login.Request;
 using MovieApi.Models.User.Login.Response;
 using MovieApi.services;
 using NUnit.Framework;
+using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MovieApiTest.Services
 {
+
     public class UserServiceTest
     {
         Mock<IUserAdapter> _mockUserAdapter;
-        Mock<HttpClient> _mockHttpClient;
+        HttpClient _mockHttpClient;
+        Mock<HttpMessageHandler> _mockHttpMessageHandler;
         Mock<IOptions<TheMoviedbSettingsModel>> _theMovieSettings;
+
 
         CreateRequestTokenResponseModel _createRequestTokenResponse = new CreateRequestTokenResponseModel();
         LoginResponseModel _loginResponse = new LoginResponseModel();
@@ -29,24 +35,30 @@ namespace MovieApiTest.Services
         public void Setup()
         {
             //dependencies (mock) + spy
-            _mockHttpClient = new Mock<HttpClient>();
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
             _mockUserAdapter = new Mock<IUserAdapter>();
             _theMovieSettings = new Mock<IOptions<TheMoviedbSettingsModel>>();
 
             HttpResponseMessage toLoginRequest = null;
             HttpResponseMessage toRequestTokenRequest = null;
-            string getAsyncRequest = null;
 
             //mock userAdapter
             _mockUserAdapter.Setup(repo => repo.ToLoginResponse(toLoginRequest))
                 .ReturnsAsync(_loginResponse);
             _mockUserAdapter.Setup(repo => repo.ToRequestTokenResponse(toRequestTokenRequest))
                 .ReturnsAsync(_createRequestTokenResponse);
-            _mockHttpClient.Setup(repo => repo.GetAsync(getAsyncRequest))
-                .ReturnsAsync(_httpMessageResponse);
+
+            _mockHttpMessageHandler.Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(_httpMessageResponse);
+
+            _mockHttpClient = new HttpClient(_mockHttpMessageHandler.Object);
             _theMovieSettings.Setup(repo => repo.Value)
                 .Returns(_theMovieSettingsResponse);
-            _service = new UserService(_mockHttpClient.Object, _mockUserAdapter.Object, _theMovieSettings.Object);
+            _service = new UserService(_mockHttpClient, _mockUserAdapter.Object, _theMovieSettings.Object);
         }
 
         [Test]
