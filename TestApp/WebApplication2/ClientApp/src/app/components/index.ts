8c081cf933eb1e4979a6a1147fa2ecfc4304eef0
Antonio Sanjuan Prieto <tonysanj@hotmail.com>
2021-02-07 12:04:52 +0100

    feat: paginator added to app-module

--- TestApp/WebApplication2/ClientApp/src/app/components/index.ts
+++ TestApp/WebApplication2/ClientApp/src/app/components/index.ts
@@ -1,4 +1,5 @@
 import { MovieBasicInfoComponent } from './common/movie-basic-info/movie-basic-info.component';
+import { PaginatorComponent } from './common/paginator/paginator.component';
 import { TopRatedMovieSearchComponent } from './movieSearchs/topRatedMovieSearch/topRatedMovieSearch/topRatedMovieSearch.component';
 import { NavMenuComponent } from './nav-bar/components/nav-menu/nav-menu.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -7,5 +8,6 @@ export const COMPONENTS: any[] = [
     NavBarComponent,
     NavMenuComponent,
     TopRatedMovieSearchComponent,
-    MovieBasicInfoComponent
+    MovieBasicInfoComponent,
+    PaginatorComponent
 ];
