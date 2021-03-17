2209696caa3e9a1f9dc78a04b77636598ddee2d2
Tony <Tony@DESKTOP-ENEUN8I>
2021-03-17 12:39:03 +0100

    text: refix

--- TestApp/WebApplication2/ClientApp/src/app/components/common/carrousel/carrousel.component.spec.ts
+++ TestApp/WebApplication2/ClientApp/src/app/components/common/carrousel/carrousel.component.spec.ts
@@ -1,12 +1,12 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { CarrouselComponent } from './carrousel.component';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-describe('FullScreenImageComponent', () => {
+describe('CarrouselComponent', () => {
   let component: CarrouselComponent;
   let fixture: ComponentFixture<CarrouselComponent>;
 
